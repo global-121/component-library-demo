fe770d5b10d187e118964d63e728d42ec9600599
Domenico Gemoli <domenicogemoli@gmail.com>
2024-05-17 10:58:11 +0200

    Add eslint and workflow file

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintPluginPrettierRecommended,
+];
