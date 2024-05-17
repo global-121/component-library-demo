ba5c5e5b995c74a038799dfe8098035f8df2a850
Domenico Gemoli <domenicogemoli@gmail.com>
2024-05-17 16:38:44 +0200

    Add spartan/ui

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { BrnAccordionContentComponent } from '@spartan-ng/ui-accordion-brain';
+import {
+  HlmAccordionContentDirective,
+  HlmAccordionDirective,
+  HlmAccordionIconDirective,
+  HlmAccordionItemDirective,
+  HlmAccordionTriggerDirective,
+} from '@spartan-ng/ui-accordion-helm';
+import { HlmIconComponent } from '@spartan-ng/ui-icon-helm';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [
+    BrnAccordionContentComponent,
+    HlmAccordionDirective,
+    HlmAccordionItemDirective,
+    HlmAccordionTriggerDirective,
+    HlmAccordionContentDirective,
+    HlmAccordionIconDirective,
+    HlmIconComponent,
+  ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
