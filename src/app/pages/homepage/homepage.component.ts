26c9d66ce006344a17109a59c470d4907081ed2d
Domenico Gemoli <domenicogemoli@gmail.com>
2024-05-17 14:43:30 +0200

    Add Material Design

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule, MatTabsModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
