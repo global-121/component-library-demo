14789e2073fcb3b3ab1df47780d3f0a353473278
Domenico Gemoli <domenicogemoli@gmail.com>
2024-05-17 15:03:59 +0200

    Add NG-Zorro

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [NzButtonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
